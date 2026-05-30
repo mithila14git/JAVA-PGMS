ec433b2aa176f2881dcf06f6f2f553f590b63c52
mithila14git <mithilakrishnak@gmail.com>
2026-05-30 20:55:21 +0530

    Added Car class program

---
+++ Car.java
@@ -0,0 +1,34 @@
+class Car {
+    // Attributes
+    String brand;
+    String model;
+    int year;
+
+    // Method to display car details
+    void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
+    // Method to start the car
+    void startCar() {
+        System.out.println(brand + " " + model + " is starting...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating an object
+        Car car1 = new Car();
+
+        // Assigning values
+        car1.brand = "Toyota";
+        car1.model = "Camry";
+        car1.year = 2024;
+
+        // Calling methods
+        car1.displayDetails();
+        car1.startCar();
+    }
+}
